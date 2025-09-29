bc41653ffba6a161cf54f8187431e09e346fd3f1
Henrique Barsante <henriquebarsante@gmail.com>
2025-09-29 19:34:16 -0300

    Inicio da tarefa, inserção dos arquivos vindo da pratica 5

---
+++ public/app.js
@@ -0,0 +1,12 @@
+const dados = [
+    {
+      "id": 1,
+      "titulo": "Prefeitura Lança Plano de Mobilidade Urbana",
+      "descricao": "Novo plano do transporte público.",
+      "conteudo": "A Prefeitura apresentou nesta segunda-feira um novo plano de mobilidade urbana.",
+      "categoria": "Cidades",
+      "autor": "Joana Ribeiro",
+      "data": "2025-03-30",
+      "imagem": "img/mobilidade.jpg"
+    }
+  ]
